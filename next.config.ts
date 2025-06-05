4bb6b83e645f6ac2386cb963bbf1d1784b8891a9
czaher <corey.zaher@gmail.com>
2025-06-05 12:00:27 -0400

    initial site setup with next

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from 'next'
+
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
+}
+export default nextConfig
